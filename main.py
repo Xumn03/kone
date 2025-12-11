dfb3677db717dd5443c7809a590b88947d969e6a
Okwus <xumn03@hotmail.com>
2025-12-11 19:40:43 -0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+Player.onPlayerSpawn(function (player) {
+	
+})
+System.onGameTick(function () {
+	
+})
+let myPlayer: Player = null
+Player.give(myPlayer, Data.itemPicker(MinecraftItemTypes.AmethystShard))
+Player.give(myPlayer, Data.itemPicker(MinecraftItemTypes.CopperIngot))
